a2a110173764c9ff6babc85cc839ca1a69cededb
Peter <peter.k.nets@gmail.com>
2015-01-09 06:29:00 +0000

    Various external code

--- js/script.js
+++ js/script.js
@@ -4,3 +4,11 @@ function Hello(){
 function EventHandler(){
     alert("I'm event handler!!");
 }
+
+alert("Hello, World!");
+
+var surname = prompt('GreetingSs friend, may I enquire as to your surname?');
+
+var name = "Dombo";
+
+var age = 20;
