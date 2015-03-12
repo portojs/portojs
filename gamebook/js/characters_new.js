8ccfe22cbcd1e5eff01094e552e6b4f19853a742
Peter <peter.k.nets@gmail.com>
2015-03-12 11:03:51 +0100

    Gamebook day-37-2

--- gamebook/js/characters_new.js
+++ gamebook/js/characters_new.js
@@ -1,3 +1,37 @@
 /**
  * Created by Peter on 12.03.2015.
  */
+hero_1 = {
+    name: 'Boromir',
+    move: 6
+};
+
+hero_2 = {
+    name: 'Aragorn',
+    move: 8
+};
+
+hero_3 = {
+    name: 'Frodo',
+    move: 4
+};
+
+hero_4 = {
+    name: 'Gandalf',
+    move: 6
+};
+
+enemy_1 = {
+    name: 'Goblin',
+    move: 7
+};
+
+enemy_2 = {
+    name: 'Orc',
+    move: 6
+};
+
+enemy_3 = {
+    name: 'Urukhai',
+    move: 5
+};
