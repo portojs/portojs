4f0202679ccb5d45cfbffe69b617375160d625fb
Peter <peter.k.nets@gmail.com>
2015-03-28 18:45:32 +0100

    Jquery Try day-2

--- gamebook_jquery/main.js
+++ gamebook_jquery/main.js
@@ -2,5 +2,8 @@
  * Created by Peter on 27.03.2015.
  */
 $(document).ready(function() {
-
+    var chaptersMenu = $("#chapters");
+    chaptersMenu.find("li");
+    chaptersMenu.first("li");
+    chaptersMenu.last("li");
 });
