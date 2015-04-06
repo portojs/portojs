85f412d7278f294ec00410a60289a0f325557b4b
Peter <peter.k.nets@gmail.com>
2015-04-06 10:45:07 +0100

    Jquery Try day-3

--- gamebook_jquery/main.js
+++ gamebook_jquery/main.js
@@ -2,8 +2,14 @@
  * Created by Peter on 27.03.2015.
  */
 $(document).ready(function() {
-    var chaptersMenu = $("#chapters");
-    chaptersMenu.find("li");
-    chaptersMenu.first("li");
-    chaptersMenu.last("li").text("Angmar");
+    $("#rangerVision").on('click', function(){
+        var enemy = $("#enemy");
+        var command1 = $("<li>Call Ranger's Speed</li>");
+        var command2 = $("<li>Call Ranger's Strength</li>");
+        var enemyDescription = $("<p>It has a limp and seems to be uncertain. There is also a gap in it's armored skin, that could be used.</p>");
+        $(this).closest(".actions").append(command1, command2);
+        $(this).remove();
+        enemy.find("p").remove();
+        enemy.append(enemyDescription);
+    });
 });
