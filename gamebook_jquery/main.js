166086b7cd3a7de60c51f84ddc5cf61331859396
Peter <peter.k.nets@gmail.com>
2015-04-08 06:04:47 +0100

    JQuery Try day-3

--- gamebook_jquery/main.js
+++ gamebook_jquery/main.js
@@ -2,14 +2,18 @@
  * Created by Peter on 27.03.2015.
  */
 $(document).ready(function() {
-    $("#rangerVision").on('click', function(){
+    $(".actions").on("click", "#rangerVision", function(){
         var enemy = $("#enemy");
+        var enemyHealth = enemy.data("hp");
         var command1 = $("<li>Call Ranger's Speed</li>");
         var command2 = $("<li>Call Ranger's Strength</li>");
-        var enemyDescription = $("<p>It has a limp and seems to be uncertain. There is also a gap in it's armored skin, that could be used.</p>");
+        var enemyDescription = $("<p>There is also a gap in it's armored skin, that could be used. Its health: " + enemyHealth + " HP.</p>");
         $(this).closest(".actions").append(command1, command2);
         $(this).remove();
         enemy.find("p").remove();
         enemy.append(enemyDescription);
+    });
+    $("herolist").on("click", "#coldeyes", function() {
+
     });
 });
