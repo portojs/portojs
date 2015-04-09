07b08144a2b355d48854b0a5b16fe72b3869b0ca
Peter <peter.k.nets@gmail.com>
2015-04-09 11:31:39 +0100

    Jquery Try day-5

--- gamebook_jquery/main.js
+++ gamebook_jquery/main.js
@@ -2,12 +2,26 @@
  * Created by Peter on 27.03.2015.
  */
 $(document).ready(function() {
+    var details = $(".details");
     var heroList = $(".herolist");
     var action = $(".action");
+    var enemy = $("#enemy");
+
+    // hiding things for later
+    details.filter(".coldeyes").hide();
+    details.filter(".bloodyaxe").hide();
+    details.filter(".shadowskill").hide();
+
+    // functions
+    function showHeroDetails() {
+        details.filter(".bloodyaxe").hide();
+        details.filter(".shadowskill").hide();
+        details.filter(".coldeyes").hide();
+        details.filter(".coldeyes").slideToggle();
+    }
 
     // calling Ranger Vision
     $(".actions").on("click", "#rangerVision", function(){
-        var enemy = $("#enemy");
         var enemyHealth = enemy.data("hp");
         var command1 = $("<li>Call Ranger's Speed</li>");
         var command2 = $("<li>Call Ranger's Strength</li>");
@@ -23,13 +37,25 @@ $(document).ready(function() {
         action.filter(".highlight").removeClass("highlight");
         action.filter(".coldeyes").addClass("highlight");
     });
-    heroList.on("click", "#bloodyaxe", function() {
+    heroList.on("mouseenter", "#coldeyes", showHeroDetails);
+    heroList.on("mouseleave", "#coldeyes", function() {
+        details.filter(".coldeyes").slideToggle();
+    });
+        heroList.on("click", "#bloodyaxe", function() {
         action.filter(".highlight").removeClass("highlight");
         action.filter(".bloodyaxe").addClass("highlight");
+    });
+    heroList.on("mouseenter", "#bloodyaxe", showHeroDetails);
+    heroList.on("mouseleave", "#bloodyaxe", function() {
+        details.filter(".bloodyaxe").slideToggle();
     });
     heroList.on("click", "#shadowskill", function() {
         action.filter(".highlight").removeClass("highlight");
         action.filter(".shadowskill").addClass("highlight");
+    });
+    heroList.on("mouseenter", "#shadowskill", showHeroDetails);
+    heroList.on("mouseleave", "#shadowskill", function() {
+        details.filter(".shadowskill").slideToggle();
     });
 
 });
