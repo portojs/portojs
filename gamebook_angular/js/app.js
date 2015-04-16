84afd995e3634ccb9087aee1f0b161eba82573bd
peter <peter.k.nets@gmail.com>
2015-04-16 12:02:15 +0300

    Angular day-1-2

--- gamebook_angular/js/app.js
+++ gamebook_angular/js/app.js
@@ -1,4 +1,16 @@
 /**
  * Created by Peter on 16.04.2015.
  */
-var app = angular.module('ideas', []);
+(function() {
+    var app = angular.module('store', []);
+
+    app.controller('storeController', function() {
+        this.product = gem;
+    });
+
+    var gem = {
+        name: 'Ruby',
+        price: 2,
+        desc: 'Great gem'
+    }
+})();
