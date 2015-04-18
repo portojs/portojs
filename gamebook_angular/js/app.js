b6d9c1898dadffe2b6400c93b54c767dbedc26da
Peter <peter.k.nets@gmail.com>
2015-04-18 10:38:24 +0100

    Angular day-3

--- gamebook_angular/js/app.js
+++ gamebook_angular/js/app.js
@@ -5,23 +5,25 @@
     var app = angular.module('dreams', []);
 
     app.controller('DreamsController', function() {
-        this.products = gems;
+        this.mercenaries = army;
     });
 
-    var gems = [
+    var army = [
         {
-            name: 'Ruby',
-            price: 20,
-            description: 'Great gem',
-            canPurchase: true,
-            soldOut: false
+            name: 'Snake',
+            price: 1799.99,
+            description: 'Top-level pro, excellent marksman and accomplished tactic',
+            canPurchase: false,
+            missedInAction: false,
+            images: ["pics/Chuck.jpg","pics/Gunman.jpg"]
         },
         {
-            name: 'Amethist',
-            price: 1,
-            description: 'Poor gem',
+            name: 'Fidel',
+            price: 1200,
+            description: 'Specialist in explosives and close-quarters combat',
             canPurchase: true,
-            soldOut: false
+            missedInAction: false,
+            images: ["pics/Thug.jpg","pics/Gunman.jpg"]
         }
     ];
 })();
