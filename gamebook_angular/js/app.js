d37c42e488450698cbb699bad9780f1824e0b54e
Peter <peter.k.nets@gmail.com>
2015-04-21 09:51:43 +0100

    Angular day-62

--- gamebook_angular/js/app.js
+++ gamebook_angular/js/app.js
@@ -8,6 +8,12 @@
         this.mercenaries = army;
     });
 
+    app.controller('GalleryController', function() {
+        this.current = 0;
+        this.setCurrent = function(newValue) {
+            this.current = newValue;
+        }
+    });
     app.controller('TabController', function() {
         this.tab = 1;
         this.setTab = function(newValue) {
@@ -24,7 +30,17 @@
             price: 1799.99,
             description: 'Top-level pro, excellent marksman and accomplished tactic',
             specifications: 'Accuracy: 85',
-            reviews: 'This merc is an asshole!',
+            reviews: [{
+                stars: 3,
+                body: "I think this merc is too expensive.",
+                author: "JimmyDean@example.org",
+                createdOn: 1397490980837
+            }, {
+                stars: 4,
+                body: "Solid skills, true professional!",
+                author: "gemsRock@example.org",
+                createdOn: 1397490980837
+            }],
             canPurchase: false,
             missedInAction: false,
             images: ["pics/Chuck.jpg","pics/Gunman.jpg"]
@@ -34,7 +50,17 @@
             price: 1200,
             description: 'Specialist in explosives and close-quarters combat',
             specifications: 'Accuracy: 75',
-            reviews: "Like this guy! Sings like an angel!",
+            reviews: [{
+                stars: 3,
+                body: "I think this merc was just OK, could honestly use more training, IMO.",
+                author: "JimmyDean@example.org",
+                createdOn: 1397490980837
+            }, {
+                stars: 4,
+                body: "Any mercenary who knows 12 ways to blow up a bridge is for me!",
+                author: "gemsRock@example.org",
+                createdOn: 1397490980837
+            }],
             canPurchase: true,
             missedInAction: false,
             images: ["pics/Thug.jpg","pics/Gunman.jpg"]
