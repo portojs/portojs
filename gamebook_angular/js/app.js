6117e6766457f9b72bd59b6c887bbbd1c83e4e2a
Peter <peter.k.nets@gmail.com>
2015-04-24 09:02:45 +0100

    Angular day-8

--- gamebook_angular/js/app.js
+++ gamebook_angular/js/app.js
@@ -32,19 +32,47 @@
         };
     });
 
-    app.directive('MercTitle', function() {
+    app.directive('mercTitle', function() {
         return {
             restrict: 'E',
             templateUrl: 'merc-title.html'
         };
     });
 
+    app.directive('mercSpecs', function() {
+        return {
+            restrict: 'A',
+            templateUrl: 'merc-specs.html'
+        };
+    });
+
+    app.directive('mercTabs', function() {
+        return {
+            restrict: 'E',
+            templateUrl: 'merc-tabs.html',
+            controller: function () {
+                this.tab = 1;
+                this.setTab = function (newValue) {
+                    this.tab = newValue;
+                };
+                this.isSet = function (tabName) {
+                    return this.tab === tabName;
+                };
+
+            },
+            controllerAs: 'tab'
+        };
+    });
     var army = [
         {
             name: 'Snake',
             price: 1799.99,
             description: 'Top-level pro, excellent marksman and accomplished tactic',
             specifications: 'Accuracy: 85',
+            age: 36,
+            accuracy: 90,
+            intellect: 80,
+            kills: 216,
             reviews: [{
                 stars: 3,
                 body: "I think this merc is too expensive.",
@@ -65,6 +93,10 @@
             price: 1200,
             description: 'Specialist in explosives and close-quarters combat',
             specifications: 'Accuracy: 75',
+            age: 41,
+            accuracy: 75,
+            speed: 70,
+            kills: 82,
             reviews: [{
                 stars: 3,
                 body: "I think this merc was just OK, could honestly use more training, IMO.",
