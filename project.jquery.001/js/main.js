beb759ee3c69e5e4222db0c5f5645bf9d2cf5e9a
Peter <peter.k.nets@gmail.com>
2015-05-13 14:15:12 +0100

    Jquery day-11

--- project.jquery.001/js/main.js
+++ project.jquery.001/js/main.js
@@ -2,9 +2,21 @@
  * Created by Peter on 12.05.2015.
  */
 $(document).ready(function(){
-    $('button').on('click', function(){
-        var quoteReply = $('<p>Are you sure?</p>');
-        $(this).closest('.decision').append(quoteReply);
+    var filters = $('#filters');
+    var decision = $('.decision');
+    decision.on('click', 'button', function(){
+        var decision = $(this).closest('.decision');
+        var amount = decision.data('price');
+        var quoteReply = $('<p>From $' + amount + '</p>');
+        decision.append(quoteReply);
         $(this).remove();
+    });
+    filters.on('click', '.standard-filter', function() {
+        $('.highlighted').removeClass('highlighted');
+        decision.filter('.standard').addClass('highlighted');
+    });
+    filters.on('click', '.exclusive-filter', function() {
+        $('.highlighted').removeClass('highlighted');
+        decision.filter('.exclusive').addClass('highlighted');
     });
 });
