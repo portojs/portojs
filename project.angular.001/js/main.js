f36b0ebe5c24ccca1c0de5b3e1c61ef37739547b
Peter <peter.k.nets@gmail.com>
2015-05-13 15:41:33 +0100

    Angular day-14

--- project.angular.001/js/main.js
+++ project.angular.001/js/main.js
@@ -14,6 +14,12 @@
             price: 2000,
             description: 'You always need to know where you stand! ' +
             'This mission allows you to have all the basic information on your enemy!',
+            images: [
+                {full: 'pics/Chuck.jpg', thumb: 'pics/Chuck_thumb.jpg'},
+                {full: 'pics/Gunman.jpg', thumb: 'pics/Gunman_thumb.jpg'},
+                {full: 'pics/Gunwoman.jpg', thumb: 'pics/Gunwoman_thumb.jpg'},
+                {full: 'pics/Thug.jpg', thumb: 'pics/Thug_thumb.jpg'}
+            ],
             canPurchase: true,
             soldOut: false
         },
@@ -22,6 +28,40 @@
             price: 3000,
             description: 'Basic information is all fine, ' +
             'but surely you need more to plan your offensives and defensives!',
+            images: [
+                {full: 'pics/Gunman.jpg', thumb: 'pics/Gunman_thumb.jpg'},
+                {full: 'pics/Chuck.jpg', thumb: 'pics/Chuck_thumb.jpg'},
+                {full: 'pics/Gunwoman.jpg', thumb: 'pics/Gunwoman_thumb.jpg'},
+                {full: 'pics/Thug.jpg', thumb: 'pics/Thug_thumb.jpg'}
+            ],
+            canPurchase: true,
+            soldOut: false
+        },
+        {
+            name: 'Recon-3',
+            price: 6000,
+            description: 'This gives you the most detailed information on ' +
+            'your adversaries. Fear no more - you will never be caught unprepared!',
+            images: [
+                {full: 'pics/Gunwoman.jpg', thumb: 'pics/Gunwoman_thumb.jpg'},
+                {full: 'pics/Gunman.jpg', thumb: 'pics/Gunman_thumb.jpg'},
+                {full: 'pics/Chuck.jpg', thumb: 'pics/Chuck_thumb.jpg'},
+                {full: 'pics/Thug.jpg', thumb: 'pics/Thug_thumb.jpg'}
+            ],
+            canPurchase: true,
+            soldOut: false
+        },
+        {
+            name: 'Assault-1',
+            price: 3000,
+            description: "The most simple kind of attacks. It won't allow you to defeat" +
+            "you enemy or capture anything, but is very useful for testing your opponent.",
+            images: [
+                {full: 'pics/Thug.jpg', thumb: 'pics/Thug_thumb.jpg'},
+                {full: 'pics/Gunman.jpg', thumb: 'pics/Gunman_thumb.jpg'},
+                {full: 'pics/Gunwoman.jpg', thumb: 'pics/Gunwoman_thumb.jpg'},
+                {full: 'pics/Chuck.jpg', thumb: 'pics/Chuck_thumb.jpg'}
+            ],
             canPurchase: true,
             soldOut: false
         }
