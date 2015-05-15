0cf6ae26e0c59a9027940c516f7351911fc47cdb
Peter <peter.k.nets@gmail.com>
2015-05-15 09:39:27 +0100

    Angular day-16

--- project.angular.001/js/main.js
+++ project.angular.001/js/main.js
@@ -8,15 +8,38 @@
         this.products = missions;
     });
 
-    app.controller('PanelController', function() {
-        this.tab = 1;
-        this.selectTab = function(setTab) {
-            this.tab = setTab;
-        };
-        this.isSelectecd = function(checkTab) {
-            return this.tab === checkTab;
+    app.controller('ReviewController', function() {
+        this.review = {};
+        this.addReview = function(product) {
+            product.reviews.push(this.review);
+            this.review = {};
         };
     });
+
+    app.directive('productTitle', function() {
+        return {
+            restrict: 'E',
+            templateUrl: 'templates/product-title.html'
+        };
+    });
+
+    app.directive('productPanels', function() {
+        return {
+            restrict: 'E',
+            templateUrl: 'templates/product-panels.html',
+            controller: function() {
+                this.tab = 1;
+                this.selectTab = function(setTab) {
+                    this.tab = setTab;
+                };
+                this.isSelected = function(checkTab) {
+                    return this.tab === checkTab;
+                };
+            },
+            controllerAs: 'panels'
+        };
+    });
+
     var missions = [
         {
             name: 'Recon',
