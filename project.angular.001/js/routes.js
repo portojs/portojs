6fd279bbb3b213da218d6b37c0c6c044a341dcae
Peter <peter.k.nets@gmail.com>
2015-05-18 16:44:59 +0100

    Angular day-19

--- project.angular.001/js/routes.js
+++ project.angular.001/js/routes.js
@@ -2,4 +2,15 @@
  * Created by Peter on 17.05.2015.
  */
 angular.module('store')
-    .config(function($routeProvider) {});
+    .config(function($routeProvider) {
+        $routeProvider.when('/decisions', {
+            templateUrl: 'templates/pages/decisions/index.html'
+        })
+        .when('/users', {
+            templateUrl: 'templates/pages/users/index.html'
+        })
+        .when('/', {
+            templateUrl: 'templates/pages/decisions/index.html'
+        })
+        .otherwise({redirectTo: '/'});
+    });
