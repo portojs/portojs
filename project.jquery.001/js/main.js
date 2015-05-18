f73ce95386f66ee44c036012ece34347b2152537
Peter <peter.k.nets@gmail.com>
2015-05-18 12:41:28 +0100

    Jquery day-16

--- project.jquery.001/js/main.js
+++ project.jquery.001/js/main.js
@@ -1,33 +1,26 @@
 /**
  * Created by Peter on 12.05.2015.
  */
-$(document).ready(function(){
+var decision = {
+    init: function(){
+        var filters = $('#filters');
+        var decision = $('.decision');
 
-    var filters = $('#filters');
-    var decision = $('.decision');
-
-    function showDecisionDetails(){
-        $(this).closest('.decision').find('.decision-details').slideToggle();
-    }
-
-    decision.on('click', '.availability-button', function(){
-        var closestDecision = $(this).closest('.decision');
-        var amount = decision.data('price');
-        var quoteReply = $('<p>Available from $' + amount + '</p>');
-        closestDecision.append(quoteReply);
-        $(this).remove();
-    });
-    filters.on('click', '.standard-filter', function() {
-        $('.highlighted').removeClass('highlighted');
-        decision.filter('.standard').addClass('highlighted');
-    });
-    filters.on('click', '.exclusive-filter', function() {
-        $('.highlighted').removeClass('highlighted');
-        decision.filter('.exclusive').addClass('highlighted');
-    });
-    /* AJAX testing*/
-    decision.on('click', '.details-button', showDecisionDetails);
-    decision.on('click', '.details-button', function() {
+        decision.on('click', '.availability-button', this.showAvailability);
+        filters.on('click', '.standard-filter', this.addHighlightStandard);
+        filters.on('click', '.exclusive-filter', this.addHighlightExclusive);
+        /* AJAX testing*/
+        decision.on('click', '.details-button', this.showDecisionDetails);
+        decision.on('click', '.details-button', this.loadDetails);
+        decision.on('click', '.details-button2', this.loadDetails2);
+        decision.on('click', '.decision-details-2-sneaky', function() {
+            $(this).addClass('highlighted');
+        });
+        decision.on('keyup', '.quantity', this.calculateTotalPrice);
+        decision.on('click', '.expand', this.showComments);
+        $('#decisions').on('click', '.decision', this.animateDecision);
+    },
+    loadDetails: function() {
         var test = $(this);
         $.ajax('html/decision-details.html', {
             success: function(response) {
@@ -40,35 +33,51 @@ $(document).ready(function(){
             beforeSend: function(){},
             complete: function(){}
         });
-    });
-    decision.on('click', '.details-button2', function() {
+    },
+    loadDetails2: function() {
         var test = $(this);
         $.get('html/decision-details-2.html', function(response) {
             test.closest('.decision').find('.decision-details-test-2').html(response).slideToggle();
         })
-    });
-    $('.decision .decision-details-2-sneaky').on('click', function() {
-        $(this).addClass('highlighted');
-    });
-//    decision.on('mouseenter', 'h3', showDecisionDetails);
-    decision.on('keyup', '.quantity', function(){
+    },
+    showAvailability: function() {
+        var closestDecision = $(this).closest('.decision');
+        var amount = decision.data('price');
+        var quoteReply = $('<p>Available from $' + amount + '</p>');
+        closestDecision.append(quoteReply);
+        $(this).remove();
+    },
+    showDecisionDetails: function() {
+        $(this).closest('.decision').find('.decision-details').slideToggle();
+    },
+    addHighlightStandard: function() {
+        $('.highlighted').removeClass('highlighted');
+        decision.filter('.standard').addClass('highlighted');
+    },
+    addHighlightExclusive: function() {
+        $('.highlighted').removeClass('highlighted');
+        decision.filter('.exclusive').addClass('highlighted');
+    },
+    calculateTotalPrice: function() {
         var price = +$(this).closest('.decision').data('price');
         var quantity = +$(this).val();
         $(this).closest('.decision').find('.total').text(price * quantity);
-    });
-    decision.on('click', '.expand', function(event){
+    },
+    showComments: function() {
         event.preventDefault();
         $(this).closest('.decision').find('.comments').fadeToggle();
-    });
-    $('#decisions').on('click', '.decision', function(){
+    },
+    animateDecision: function() {
         $(this).toggleClass('highlighted');
-
         if ($(this).hasClass('highlighted')) {
             $(this).animate({'top': '-10px'}, 'fast');
         }
         else {
             $(this).animate({'top': '0px'}, 'fast');
         }
+    }
+};
 
-    });
+$(document).ready(function(){
+    decision.init();
 });
